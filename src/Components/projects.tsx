6020b9002c32710c4d82549133ddd6cd7c6d8d00
jcstein <46639943+jcstein@users.noreply.github.com>
2025-05-06 18:01:06 -0400

    style: responsive

--- src/Components/projects.tsx
+++ src/Components/projects.tsx
@@ -4,79 +4,54 @@ import {
   VStack,
   Button,
   Image,
+  Box,
 } from "@chakra-ui/react";
+import "../gridStyles.css";
 import { FaInstagram } from "react-icons/fa";
+
+const ImageGrid = () => {
+  // Create array of image sources with size classes
+  const images = [
+    { src: "./1.png", alt: "1 description", className: "grid-item grid-item-medium" },
+    { src: "./3.png", alt: "3 description", className: "grid-item grid-item-large" },
+    { src: "./6.png", alt: "6 description", className: "grid-item grid-item-small" },
+    { src: "./2.png", alt: "2 description", className: "grid-item grid-item-medium" },
+    { src: "./5.png", alt: "5 description", className: "grid-item grid-item-large" },
+    { src: "./4.png", alt: "4 description", className: "grid-item grid-item-small" },
+    { src: "./7.png", alt: "7 description", className: "grid-item grid-item-medium" },
+    { src: "./8.png", alt: "8 description", className: "grid-item grid-item-large" },
+  ];
+
+  return (
+    <div className="image-grid">
+      {images.map((image, index) => (
+        <div 
+          key={index} 
+          className={image.className}
+        >
+          <Image
+            src={image.src}
+            width="100%"
+            height="100%"
+            objectFit="cover"
+            rounded="2xl"
+            alt={image.alt}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export const Projects = () => {
   return (
-    <VStack>
+    <VStack spacing={6} width="100%">
       <Heading size="lg" textAlign="center" pt="3">
         My work
       </Heading>
-      <Image
-        src="./1.png"
-        width="90%"
-        maxW="700px"
-        rounded="2xl"
-        alt="1 description"
-        pb="3"
-      />
-      <Image
-        src="./2.png"
-        width="90%"
-        maxW="700px"
-        rounded="2xl"
-        alt="2 description"
-        pb="3"
-      />
-      <Image
-        src="./3.png"
-        width="90%"
-        maxW="700px"
-        rounded="2xl"
-        alt="3 description"
-        pb="3"
-      />
-       <Image
-        src="./4.png"
-        width="90%"
-        maxW="700px"
-        rounded="2xl"
-        alt="4 description"
-        pb="3"
-      />
-       <Image
-        src="./5.png"
-        width="90%"
-        maxW="700px"
-        rounded="2xl"
-        alt="5 description"
-        pb="3"
-      />
-       <Image
-        src="./6.png"
-        width="90%"
-        maxW="700px"
-        rounded="2xl"
-        alt="6 description"
-        pb="3"
-      />
-       <Image
-        src="./7.png"
-        width="90%"
-        maxW="700px"
-        rounded="2xl"
-        alt="7 description"
-        pb="3"
-      />
-       <Image
-        src="./8.png"
-        width="90%"
-        maxW="700px"
-        rounded="2xl"
-        alt="8 description"
-        pb="3"
-      />
+      
+      <ImageGrid />
+      
       <Divider />
       <Heading size="md" textAlign="center" pt="3">
         Stay tuned on social media
